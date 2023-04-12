82928f9b3aa93c7ebcdaa310f4272d8639d96d56
Pham Nhat Tien (DC.ODC) <tienpn@inter-k.com>
2023-04-12 17:36:42 +0700

    with another

--- services/main/src/pages/nextapp.page.tsx
+++ services/main/src/pages/nextapp.page.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "preact/hooks";
+import { useEffect, useLayoutEffect, useRef } from "preact/hooks";
 import { signal } from "@preact/signals";
 import { useRouter } from "preact-router";
 
@@ -7,21 +7,18 @@ export function Neactapp() {
   const match = Object.values(router.matches)[0];
   console.log(match);
 
-  const iframeSrc = signal(`http://localhost:3000/${match}`);
+  const src = signal(`http://localhost:3000/${match}`);
+  useLayoutEffect(() => {
+    async function renderMyPage() {
+      const response = await fetch(`http://localhost:3000/${match}`);
+      const html = await response.text();
+      console.log(html);
+      // Hiển thị HTML đã được render trong ứng dụng của bạn
+      // document.getElementById("nextapp").innerHTML = html;
+    }
 
-  // useEffect(() => {
-  //   const listenHandler = () => {
-  //     // console.log(window.location.pathname);
-  //   };
-  //   window.addEventListener("app:route:change", listenHandler);
-  //   return () => {
-  //     window.removeEventListener("app:route:change", listenHandler);
-  //   };
-  // }, []);
+    renderMyPage();
+  });
 
-  return (
-    <div class="nextapp">
-      <iframe allow="fullscreen" src={iframeSrc} frameBorder="0"></iframe>
-    </div>
-  );
+  return <div id="nextapp"></div>;
 }
